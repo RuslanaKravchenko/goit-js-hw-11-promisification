4b40d7ca7c0937be097adc7e23b973d19f349e28
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-12-09 09:43:50 +0200

    Вносит правки

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 import './styles.css';
 
 // ================== task 1 =======================
-// const delay = ms => {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve(ms);
-//     }, ms);
-//   });
-// };
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+};
 
-// const logger = time => console.log(`Resolved after ${time}ms`);
+const loggerTaskOne = time => console.log(`Resolved after ${time}ms`);
 
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+// Вызовы функции для проверки
+delay(2000).then(loggerTaskOne); // Resolved after 2000ms
+delay(1000).then(loggerTaskOne); // Resolved after 1000ms
+delay(1500).then(loggerTaskOne); // Resolved after 1500ms
 
 // ================== task 2 =======================
 
-// const users = [
-//   { name: 'Mango', active: true },
-//   { name: 'Poly', active: false },
-//   { name: 'Ajax', active: true },
-//   { name: 'Lux', active: false },
-// ];
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
 
-// const toggleUserState = (allUsers, userName) => {
-//   return new Promise(resolve => {
-//     const updatedUsers = allUsers.map(user =>
-//       user.name === userName ? { ...user, active: !user.active } : user,
-//     );
-//     resolve(updatedUsers);
-//   });
-// };
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
+  });
+};
 
-// const logger = updatedUsers => console.table(updatedUsers);
+const logger = updatedUsers => console.table(updatedUsers);
 
-// toggleUserState(users, 'Mango').then(logger);
-// toggleUserState(users, 'Lux').then(logger);
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
 
 // ================== task 3 =======================
 
